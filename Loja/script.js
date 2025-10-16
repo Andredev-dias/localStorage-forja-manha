4d780267c67f1619d3d04b79115fedefcadeb0c8
André Dias <57869108+Andredev-dias@users.noreply.github.com>
2025-10-16 11:08:28 -0300

    Add files via upload

--- Loja/script.js
+++ Loja/script.js
@@ -61,5 +61,14 @@ document.addEventListener("DOMContentLoaded", function(){
                 produtosContainer.appendChild(card)
             });
         }).catch((error) => console.log("Erro ao carregar dados", error))
+
+        $("#produtos-container").on("click", ".adicionar", function(){
+            const indexDoProduto = $(this).data("indice")
+            const produtoSelecionado = produtos[indexDoProduto]
+            let carrinho = JSON.parse(localStorage.getItem("carrinho")) || []
+            carrinho.push(produtoSelecionado)
+            localStorage.setItem("carrinho", JSON.stringify(carrinho))
+            alert("Produto adicionado com sucesso!!!")
+        })
 })
 
